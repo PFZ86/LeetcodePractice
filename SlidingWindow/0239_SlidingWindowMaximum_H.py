9bc49f1c9e7f9ca463946bf9a56b91c6e90a453d
PFZ86 <52639750+PFZ86@users.noreply.github.com>
2019-12-31 08:01:56 -0500

    Add files via upload

---
+++ SlidingWindow/0239_SlidingWindowMaximum_H.py
@@ -0,0 +1,42 @@
+# https://leetcode.com/problems/sliding-window-maximum/
+
+# Solution 1: maintain a deque of index
+#             delete from front old elements
+#             delete from back small elements
+#             (so the implicit deque of numbers will be non-increasing)
+'''
+nums = [1,3,-1,-3,5,3,6,7], and k = 3
+idx     num     idx_queue     (num_queue)            res
+  0       1             0               1
+  1       3             1               3
+  2      -1           1,2            3,-1              3
+  3      -3         1,2,3         3,-1,-3            3,3
+  4       5             4               5          3,3,5
+  5       3           4,5             5,3        3,3,5,5
+  6       6             6               6      3,3,5,5,6
+  7       7             7               7    3,3,5,5,6,7
+'''
+
+class Solution(object):
+    def maxSlidingWindow(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        idx_queue = []
+        res = []
+        for idx, num in enumerate(nums):
+            # delete from front: remove elements at least k positions before idx
+            while len(idx_queue) and idx_queue[0] <= (idx-k):
+                del idx_queue[0]
+            # delete from back: remove elements no greater than num
+            while len(idx_queue) and nums[idx_queue[-1]] <= num:
+                del idx_queue[-1]
+            # add current index
+            idx_queue.append(idx)
+
+            if idx >= (k-1):
+                res.append(nums[idx_queue[0]])
+
+        return res
