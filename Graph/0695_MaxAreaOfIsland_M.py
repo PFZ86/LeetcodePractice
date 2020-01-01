6c9ccfef5dcb2c8ba23a31e0cc735c2cf72b3324
PFZ86 <52639750+PFZ86@users.noreply.github.com>
2020-01-01 10:50:20 -0500

    Add files via upload

---
+++ Graph/0695_MaxAreaOfIsland_M.py
@@ -0,0 +1,44 @@
+# https://leetcode.com/problems/max-area-of-island/
+
+# Solution 1: dfs
+class Solution(object):
+    def maxAreaOfIsland(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        def dfs(i, j, area):
+            if i < 0 or i == nrow or j < 0 or j == ncol:
+                return area
+            if visited[i][j]:
+                return area
+
+            visited[i][j] = True
+            area += 1
+
+            area = dfs(i-1, j, area)
+            area = dfs(i+1, j, area)
+            area = dfs(i, j-1, area)
+            area = dfs(i, j+1, area)
+
+            return area
+
+        nrow, ncol = len(grid), len(grid[0])
+
+        visited = [[False] * ncol for _ in range(nrow)]
+
+        # mark 0 (water) as visited
+        for i in range(nrow):
+            for j in range(ncol):
+                if grid[i][j] == 0:
+                    visited[i][j] = True
+
+        max_area = 0
+        for i in range(nrow):
+            for j in range(ncol):
+                if not visited[i][j]:
+                    area = dfs(i, j, 0)
+                    max_area = max(max_area, area)
+
+        return max_area
+        
