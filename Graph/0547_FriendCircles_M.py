3f16a429a9fc5d8b730fa45830fef62a653d9e69
PFZ86 <52639750+PFZ86@users.noreply.github.com>
2020-01-01 09:12:36 -0500

    Add files via upload

--- Graph/0547_FriendCircles_M.py
+++ Graph/0547_FriendCircles_M.py
@@ -28,4 +28,34 @@ class Solution(object):
                 num_friend_circles += 1
 
         return num_friend_circles
+
+# Solution 2: dfs;
+class Solution(object):
+    def findCircleNum(self, M):
+        """
+        :type M: List[List[int]]
+        :rtype: int
+        """
+        def dfs(i):
+            if visited[i]:
+                return
+
+            visited[i] = True
+            for j in range(N):
+                if j != i and M[i][j] == 1:
+                    dfs(j)
+            return
+
+        N = len(M)
+        if N == 0:
+            return 0
+
+        visited = [False for _ in range(N)]
+        res = 0
+        for i in range(N):
+            if not visited[i]:
+                dfs(i)
+                res += 1
+
+        return res
         
