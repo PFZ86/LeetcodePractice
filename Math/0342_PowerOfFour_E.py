0a5ef3e04f05fb9523934e3062991484b4fed098
PFZ86 <52639750+PFZ86@users.noreply.github.com>
2020-01-05 11:24:08 -0500

    Add files via upload

--- Math/0342_PowerOfFour_E.py
+++ Math/0342_PowerOfFour_E.py
@@ -1,6 +1,6 @@
 # https://leetcode.com/problems/power-of-four/
 
-# Solution 1: the iterative method
+# Solution 1: brute force; iterative
 class Solution(object):
     def isPowerOfFour(self, n):
         """
@@ -24,4 +24,27 @@ class Solution(object):
         :type num: int
         :rtype: bool
         """
-        return n >= 1 and (n&(n-1)) == 0 and (n-1)%3 == 0
+        return n >= 1 and (n & (n-1)) == 0 and (n % 3 == 1)
+
+# Solution 3: trick
+'''
+0x55555555 = 01010101010101010101010101010101
+If n is power of two, then n has single bit 1.
+We use 0x55555555 to check if the single bit is at odd or even position.
+(n & 0x55555555) != 0
+(n & 0x55555555) == n
+(n | 0x55555555) == 0x55555555
+'''
+class Solution(object):
+    def isPowerOfFour(self, n):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        return n >= 1 and (n & (n-1)) == 0 and (n & 0x55555555 != 0)
+
+        # or
+        #return n >= 1 and (n & (n-1)) == 0 and (n & 0x55555555 == n)
+
+        # or
+        #return n >= 1 and (n & (n-1)) == 0 and (n | 0x55555555 == 0x55555555)
