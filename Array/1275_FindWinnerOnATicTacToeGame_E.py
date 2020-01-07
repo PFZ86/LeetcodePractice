bc1fb2b1cbaf52676471bc6c204c3e86b3b3c3ab
PFZ86 <52639750+PFZ86@users.noreply.github.com>
2020-01-07 12:02:11 -0500

    Update 1275_FindWinnerOnATicTacToeGame_E.py

--- Array/1275_FindWinnerOnATicTacToeGame_E.py
+++ Array/1275_FindWinnerOnATicTacToeGame_E.py
@@ -39,4 +39,62 @@ class Solution(object):
                     return player
 
         return "Draw" if len(moves) == (n**2) else "Pending"
+
+# Solution 2: naive solution
+class Solution(object):
+    def tictactoe(self, moves):
+        """
+        :type moves: List[List[int]]
+        :rtype: str
+        """
+        n = 3
+        grid = [[' '] * n for _ in range(n)]
         
+        for idx in range(len(moves)):
+            player = 'A' if idx % 2 == 0 else 'B'
+            i = moves[idx][0]
+            j = moves[idx][1]
+            
+            grid[i][j] = player
+            
+            # check column
+            cnt = 0
+            for c in range(n):
+                if grid[i][c] == player:
+                    cnt += 1
+            if cnt == n:
+                return player
+            
+            # check row
+            cnt = 0
+            for r in range(n):
+                if grid[r][j] == player:
+                    cnt += 1
+            if cnt == n:
+                return player
+            
+            # check maindiag
+            if i == j:
+                cnt = 0
+                for d in range(n):
+                    if grid[d][d] == player:
+                        cnt += 1
+                if cnt == n:
+                    return player
+                
+            # check antidiag
+            if i == (n-1-j):
+                cnt = 0
+                for d in range(n):
+                    if grid[d][n-1-d] == player:
+                        cnt += 1
+                if cnt == n:
+                    return player
+        
+        # no winner after all moves
+        
+        if len(moves) == n**2:
+            return 'Draw'
+        
+        return 'Pending'
+    
