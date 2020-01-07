0f4ecebb2f9c9bdac1454a3b1fc406db7c1add3b
PFZ86 <52639750+PFZ86@users.noreply.github.com>
2020-01-06 22:31:46 -0500

    Add files via upload

--- BinarySearchTree/0098_ValidateBinarySearchTree_M.py
+++ BinarySearchTree/0098_ValidateBinarySearchTree_M.py
@@ -7,27 +7,52 @@
 #         self.left = None
 #         self.right = None
 
-# Solution 1: recursive
+# Solution 1: the recursive solution
 class Solution(object):
-    def isValidBST_helper(self, node, low_val, upp_val):
-        if node is None:
-            return True
-
-        # node.val is out of valid range
-        if (low_val is not None and node.val <= low_val) or (upp_val is not None and node.val >= upp_val):
-            return False
-
-        # valid range for left child
-        left_valid = self.isValidBST_helper(node.left, low_val, node.val)
-        # valid range for right chold
-        right_valid = self.isValidBST_helper(node.right, node.val, upp_val)
-
-        return left_valid and right_valid
-
     def isValidBST(self, root):
         """
         :type root: TreeNode
         :rtype: bool
         """
+        def validate(node, low_bound, upp_bound):
+            if node is None:
+                return True
 
-        return self.isValidBST_helper(root, None, None)
+            # node.val is out of bound
+            if (low_bound is not None and node.val <= low_bound) or (upp_bound is not None and node.val >= upp_bound):
+                return False
+
+            return validate(node.left, low_bound, node.val) and validate(node.right, node.val, upp_bound)
+
+        return validate(root, None, None)
+
+# Solution 2: the iterative solution
+'''
+Iterative inorder traversal of binary tree
+https://github.com/PFZ86/LeetcodePractice/blob/master/Tree/BinaryTreeIterativeInOrderTraversal.py
+'''
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        visit = root
+        stack = []
+
+        prev_value = None
+        while visit is not None or len(stack) > 0:
+            while visit is not None:
+                stack.append(visit)
+                visit = visit.left
+
+            node = stack[-1]
+            del stack[-1]
+            visit = node.right
+
+            if prev_value is not None and node.val <= prev_value:
+                return False
+            prev_value = node.val
+
+        return True
+        
