e382af62f8a0620b358f04747b90ee5ac2e16c03
PFZ86 <52639750+PFZ86@users.noreply.github.com>
2020-01-07 21:48:32 -0500

    Add files via upload

--- Math/0264_UglyNumberII_M.py
+++ Math/0264_UglyNumberII_M.py
@@ -1,17 +1,19 @@
 # https://leetcode.com/problems/ugly-number-ii/
 
 # Solution 1: the Sieve method
-# idx2, idx3, idx5      ugly numbers
-#    0,    0,    0      1
-#    1,    0,    0      1, 2
-#    1,    1,    0      1, 2, 3
-#    2,    1,    0      1, 2, 3, 4
-#    2,    1,    1      1, 2, 3, 4, 5
-#    3,    2,    1      1, 2, 3, 4, 5, 6
-#    4,    2,    1      1, 2, 3, 4, 5, 6, 8
-#    4,    3,    1      1, 2, 3, 4, 5, 6, 8, 9
-#    5,    3,    2      1, 2, 3, 4, 5, 6, 8, 9, 10
-#    6,    3,    2      1, 2, 3, 4, 5, 6, 8, 9, 10, 12
+'''
+idx2, idx3, idx5      ugly numbers
+   0,    0,    0      1
+   1,    0,    0      1, 2
+   1,    1,    0      1, 2, 3
+   2,    1,    0      1, 2, 3, 4
+   2,    1,    1      1, 2, 3, 4, 5
+   3,    2,    1      1, 2, 3, 4, 5, 6
+   4,    2,    1      1, 2, 3, 4, 5, 6, 8
+   4,    3,    1      1, 2, 3, 4, 5, 6, 8, 9
+   5,    3,    2      1, 2, 3, 4, 5, 6, 8, 9, 10
+   6,    3,    2      1, 2, 3, 4, 5, 6, 8, 9, 10, 12
+'''
 
 class Solution(object):
     def nthUglyNumber(self, n):
@@ -19,20 +21,22 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        idx2, idx3, idx5 = 0, 0, 0
-        ugly_numbers = [1]
+        ugly_nums = [1]
+        i2, i3, i5 = 0, 0, 0
 
-        while len(ugly_numbers) < n:
-            tmp2 = 2 * ugly_numbers[idx2]
-            tmp3 = 3 * ugly_numbers[idx3]
-            tmp5 = 5 * ugly_numbers[idx5]
-            tmp_num = min([tmp2, tmp3, tmp5])
-            if tmp_num == tmp2:
-                idx2 += 1
-            if tmp_num == tmp3:
-                idx3 += 1
-            if tmp_num == tmp5:
-                idx5 += 1
-            ugly_numbers.append(tmp_num)
+        while len(ugly_nums) < n:
+            n2 = ugly_nums[i2] * 2
+            n3 = ugly_nums[i3] * 3
+            n5 = ugly_nums[i5] * 5
 
-        return ugly_numbers[n-1]
+            num = min(n2, min(n3, n5))
+            if num == n2:
+                i2 += 1
+            if num == n3:
+                i3 += 1
+            if num == n5:
+                i5 += 1
+
+            ugly_nums.append(num)
+
+        return ugly_nums[-1]
