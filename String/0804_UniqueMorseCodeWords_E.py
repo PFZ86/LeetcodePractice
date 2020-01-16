44a21e2e10d835fc0dcf0a46463c839b5f5a9db6
PFZ86 <52639750+PFZ86@users.noreply.github.com>
2020-01-15 22:31:03 -0500

    Add files via upload

---
+++ String/0804_UniqueMorseCodeWords_E.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/unique-morse-code-words/
+
+# Solution 1
+class Solution(object):
+    def uniqueMorseRepresentations(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
+        import string
+
+        letter_list = list(string.ascii_lowercase)
+        code_list = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+
+        letter_to_code = dict(zip(letter_list, code_list))
+
+        code_set = set()
+        for word in words:
+            code = ''
+            for letter in word:
+                code += letter_to_code[letter]
+            code_set.add(code)
+
+        return len(code_set)
+        
